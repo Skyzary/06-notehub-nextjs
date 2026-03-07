b3ef7b6715f8ef3874ca9f827ec0ff33588c33cd
levkovskyivolodymyr <levkovskii44@gmail.com>
2026-03-07 16:45:33 +0100

    refactoring

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,6 +1,6 @@
 import {dehydrate, HydrationBoundary, QueryClient} from '@tanstack/react-query';
 import {getNoteById} from '../../../lib/api';
-import NoteDetailsClient from './Note.details.client'
+import NoteDetailsClient from './Note.Details.client'
 export default async function NotePage(id: string) {
   const queryClient = new QueryClient()
 
